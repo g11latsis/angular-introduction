2bc58924479c15bb652b63115240828981310e19
Grigoris Latsis <latsis@dyg.gr>
2024-03-29 18:03:21 +0200

    Component create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   name = 'Grigoris Latsis';
-
-  person ={
-    givenName: 'Grigoris',
-    surName: "Latsis",
-    age: 31,
-    email:"glatsis@aueb.gr"
-  }
 }
